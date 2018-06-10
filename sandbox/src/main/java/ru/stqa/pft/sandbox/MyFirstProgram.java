acb916b26e03ef3d872dbc093a7c9a7edaa79371
shomrina <milagrous@gmail.com>
2018-06-11 00:19:34 +0300

    'distance' move to class Point
    distance - not static, now it is transform to method
    and get one object.
    Method 'main' have changed with new method

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -14,13 +14,9 @@ public class MyFirstProgram {
 
     Point p1 = new Point(3.0, 4.5);
     Point p2 = new Point(-2.2, 7.5);
-    System.out.println("The distance between two points = " + distance(p1, p2));
+    System.out.println("The distance between two points = " + p1.distance(p2));
 
   }
 
-  public static double distance(Point p1, Point p2) {
-    return Math.sqrt((p2.x - p1.x)*(p2.x - p1.x) + (p2.y - p1.y)*(p2.y - p1.y));
-//    return Math.sqrt(Math.pow((p2.x - p1.x), 2) + Math.pow((p2.y - p1.y), 2));
-  }
 
 }
--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -9,4 +9,10 @@ public class Point {
     this.x = x;
     this.y = y;
   }
+
+  public double distance(Point p) {
+    return Math.sqrt((p.x - this.x)*(p.x - this.x) + (p.y - this.y) *(p.y - this.y));
+  }
+
+
 }
